e4211b809f5c55f6f40030f017c297906f44a620
SachTech <sachtech@gmail.com>
2026-09-23 12:23:10 +0530

    Persist data protection keys for credential encryption

--- ServiceNowCM.Api/Program.cs
+++ ServiceNowCM.Api/Program.cs
@@ -4,11 +4,34 @@ using ServiceNowCM.Application.Services;
 using ServiceNowCM.ContentManager.Services;
 using ServiceNowCM.Infrastructure;
 using ServiceNowCM.Infrastructure.Repositories;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// ---------------------------------------------------------
+// Data Protection
+//
+// Persist encryption keys outside the source-code folder so
+// encrypted credentials remain readable even if the project
+// is moved or freshly cloned.
+// ---------------------------------------------------------
+
+var dataProtectionKeysPath = Path.Combine(
+    Environment.GetFolderPath(
+        Environment.SpecialFolder.LocalApplicationData),
+    "ServiceNowCM",
+    "DataProtectionKeys");
+
+Directory.CreateDirectory(dataProtectionKeysPath);
+
+builder.Services
+    .AddDataProtection()
+    .SetApplicationName("ServiceNowCM")
+    .PersistKeysToFileSystem(
+        new DirectoryInfo(dataProtectionKeysPath));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
